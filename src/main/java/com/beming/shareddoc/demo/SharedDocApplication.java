a70bc756bbb7fc195a575aed2082e17a24578e3c
beming-dev <mingfordev@gmail.com>
2024-05-17 14:02:52 +0900

    First commit

---
+++ src/main/java/com/beming/shareddoc/demo/SharedDocApplication.java
@@ -0,0 +1,13 @@
+package com.beming.shareddoc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedDocApplication.class, args);
+	}
+
+}
